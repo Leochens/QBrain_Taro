cae20cb4d29e9e8b54d50ff9176cd3a7a60aaf71
Leochens <18332518328@163.com>
2020-01-16 15:09:30 +0800

    [U]up

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 export const appConfig = {
 	// apiBaseUrl: 'http://10.18.93.121:8899',
-	apiBaseUrl: 'https://deecamp.chuangxin.com/weapop',
+	apiBaseUrl: 'https://deecamp.chuangxin.com/weapp',
 	// apiBaseUrl: 'http://10.18.93.121:5002/weapp',
 	// expiration: 1000 * 60 * 60 * 24 * 7, //设置SID过期时间 7天
 	// 利用微信自带的checksession 就不用设置过期时间了
